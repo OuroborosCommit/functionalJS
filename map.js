7cedb4e59745fbf9fa7c831bdf8ee1271e88b6bb
Chimp <jesse_ducharme@live.fr>
2018-03-18 20:25:41 -0400

    Exposing the itterative job of reduce

--- map.js
+++ map.js
@@ -21,9 +21,20 @@ var transformers = [
   }
 ];
 
+var orders = [
+{amount :50 },
+{amount :100 },
+{amount :150 },
+{amount :200 },
+{amount :250 },
+]
+
 var names = transformers.map((x) => x.name);
 
+var totalAmount = orders.reduce((sum,order) => {
+	console.log('Iterated :',sum,order);
+	return sum+order.amount},0)
  
 
-
-console.log(names);
+console.log('Total amount: ',totalAmount);
+console.log('List of transformers name',names);
