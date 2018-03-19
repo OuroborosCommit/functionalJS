d1679b4ac1870dfa099e4a6a353376ef283dfb01
Chimp <jesse_ducharme@live.fr>
2018-03-18 20:09:05 -0400

    Constructed the array to test on and basic map

---
+++ map.js
@@ -0,0 +1,29 @@
+var transformers = [
+  {
+    name: 'Optimus Prime',
+    form: 'Freightliner Truck',
+    team: 'Autobot'
+  },
+  {
+    name: 'Megatron',
+    form: 'Gun',
+    team: 'Decepticon'
+  },
+  {
+    name: 'Bumblebee',
+    form: 'VW Beetle',
+    team: 'Autobot'
+  },
+  {
+    name: 'Soundwave',
+    form: 'Walkman',
+    team: 'Decepticon'
+  }
+];
+
+var names = transformers.map((x) => x.name);
+
+ 
+
+
+console.log(names);
